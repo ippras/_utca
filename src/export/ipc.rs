147b0b768fea0270143176b75cad70d2710c7f5d
kgv <kgv@users.noreply.github.com>
2025-05-26 13:15:04 +0300

    Fix save data frame

--- src/export/ipc.rs
+++ src/export/ipc.rs
@@ -1,16 +1,16 @@
-use anyhow::Result;
-use metadata::MetaDataFrame;
-use tracing::instrument;
-
 #[cfg(not(target_arch = "wasm32"))]
 pub use self::native::{save, save_data};
 #[cfg(target_arch = "wasm32")]
-pub use self::web::save;
+pub use self::web::{save, save_data};
+
+use anyhow::Result;
+use metadata::MetaDataFrame;
+use polars::prelude::*;
+use tracing::instrument;
 
 #[cfg(not(target_arch = "wasm32"))]
 mod native {
     use super::*;
-    use polars::prelude::*;
     use std::fs::File;
 
     #[instrument(err)]
@@ -24,7 +24,6 @@ mod native {
     pub fn save_data(data_frame: &mut DataFrame, name: &str) -> Result<()> {
         let file = File::create(name)?;
         let mut writer = IpcWriter::new(file);
-        // writer.metadata(self.meta);
         writer.finish(data_frame)?;
         Ok(())
     }
@@ -35,11 +34,25 @@ mod web {
     use super::*;
     use anyhow::bail;
     use egui_ext::download::{NONE, download};
+    use metadata::Metadata;
 
     #[instrument(err)]
     pub fn save(frame: &mut MetaDataFrame, name: &str) -> Result<()> {
         let mut bytes = Vec::new();
-        MetaDataFrame::new(frame.meta.clone(), &mut frame.data).write(&mut bytes)?;
+        let mut writer = IpcWriter::new(&mut bytes);
+        writer.metadata(frame.meta.clone());
+        writer.finish(&mut frame.data)?;
+        if let Err(error) = download(&bytes, NONE, name) {
+            bail!("save: {error:?}");
+        }
+        Ok(())
+    }
+
+    #[instrument(err)]
+    pub fn save_data(data_frame: &mut DataFrame, name: &str) -> Result<()> {
+        let mut bytes = Vec::new();
+        let mut writer = IpcWriter::new(&mut bytes);
+        writer.finish(data_frame)?;
         if let Err(error) = download(&bytes, NONE, name) {
             bail!("save: {error:?}");
         }
